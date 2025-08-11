63da26de202e1a7520daf6754c16bae703957d52
Harish Ganesh <45884389+RYHarish@users.noreply.github.com>
2025-08-11 22:54:51 +0530

    Time: 3 ms, Memory: 14.4 MB - LeetHub

---
+++ unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def allPossibleBST(self, start, end, memo):
+        res = []
+        if start > end:
+            res.append(None)
+            return res
+        if (start, end) in memo:
+            return memo[(start, end)]
+
+        for i in range(start, end + 1):
+            leftSubTrees = self.allPossibleBST(start, i - 1, memo)
+            rightSubTrees = self.allPossibleBST(i + 1, end, memo)
+
+            for left in leftSubTrees:
+                for right in rightSubTrees:
+                    root = TreeNode(i, left, right)
+                    res.append(root)
+
+        memo[(start, end)] = res
+        return res
+
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[Optional[TreeNode]]
+        """
+        memo = {}
+        return self.allPossibleBST(1, n, memo)
